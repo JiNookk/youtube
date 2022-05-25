8404951e45c81b6e308b54bb7ea5359e2cc2186a
Jin Wook <ojw0828@naver.com>
2022-05-25 11:22:26 +0900

    init projects

---
+++ .history/src/components/App_20220517142943.js
@@ -0,0 +1,12 @@
+import "./app.css";
+
+// jsx파일을 왜쓰는가 ?
+//   순수 js로직을 다루는 파일 >> js
+//   react의 컴포넌트파일 >> jsx
+//   이 둘을 구분하기 위해 사용!
+
+function App() {
+  return <h1>Hello :)</h1>;
+}
+
+export default App;
